1a5ed59cd73828c7c117f88a958873296cc27cbf
Shafayat <shafayat1777@gmail.com>
2026-04-18 14:24:44 +0600

    fix: :bug:

--- electron/src/ui/page/signin.tsx
+++ electron/src/ui/page/signin.tsx
@@ -1,7 +1,8 @@
 import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router';
+import { useNavigate } from 'react-router';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Cloud } from 'lucide-react';
 import { toast } from 'sonner';
 
 import {
@@ -9,14 +10,7 @@ import {
     SIGNIN_DEFAULT_SCHEMA,
     SIGNIN_SCHEMA,
 } from '@/schema/signin';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from '@/ui/components/ui/card';
+import { Card, CardContent, CardDescription, CardTitle } from '@/ui/components/ui/card';
 
 import FormWrapper from '../components/form/form';
 import FormInput from '../components/form/input';
@@ -48,33 +42,50 @@ const SignIn = () => {
 
     return (
         <div className="flex h-screen items-center justify-center">
-            <Card className="w-full max-w-md">
-                <CardHeader>
+            <Card className="w-full max-w-4xl">
+                {/* <CardHeader>
                     <CardTitle>Login to your account</CardTitle>
                     <CardDescription>
                         Enter your email below to login to your account
                     </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <FormWrapper
-                        buttonText="Sign In"
-                        onSubmit={form.handleSubmit(handleClick)}
-                    >
-                        <FormInput
-                            control={form.control}
-                            name="email"
-                            label="Email"
-                            placeholder="Write your mail"
-                        />
-                        <FormInputPassword
-                            control={form.control}
-                            name="password"
-                            label="Password"
-                            placeholder="Write your password"
-                        />
-                    </FormWrapper>
+                </CardHeader> */}
+                <CardContent className="grid grid-cols-2">
+                    <div className='space-y-8'>
+                        <CardTitle className="flex items-center gap-4 font-black uppercase">
+                            <Cloud
+                                className="bg-primary h-10 w-10 rounded-full p-2 text-white shadow"
+                                fill="white"
+                            />
+                            Login to your account
+                        </CardTitle>
+                        <CardDescription className='text-3xl'>
+                            Unified data Persistence layer.
+                            <p>Access your decentralized workspace with enterprise-grade encryption amd seamless sync</p>
+                        </CardDescription>
+                    </div>
+                    <div>
+                        <div>
+                            <FormWrapper
+                                buttonText="Sign In"
+                                onSubmit={form.handleSubmit(handleClick)}
+                            >
+                                <FormInput
+                                    control={form.control}
+                                    name="email"
+                                    label="Email"
+                                    placeholder="Write your mail"
+                                />
+                                <FormInputPassword
+                                    control={form.control}
+                                    name="password"
+                                    label="Password"
+                                    placeholder="Write your password"
+                                />
+                            </FormWrapper>
+                        </div>
+                    </div>
                 </CardContent>
-                <CardFooter className="flex-col gap-2">
+                {/* <CardFooter className="flex-col gap-2">
                     <div className="mt-4 text-center text-sm">
                         Don&apos;t have an account?{' '}
                         <Link
@@ -84,7 +95,7 @@ const SignIn = () => {
                             Sign up
                         </Link>
                     </div>
-                </CardFooter>
+                </CardFooter> */}
             </Card>
         </div>
     );
